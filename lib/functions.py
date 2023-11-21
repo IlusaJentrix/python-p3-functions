3eb08a35871a72d7d8b30ad0ec870920da1d8c54
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-11-21 10:54:21 +0300

    Completed Assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print ("Hello, programmer!")
+greet_programmer() 
+def greet(name='Guido'):
+    print(f"Hello, {name}!")
+greet()  
 
-def greet(name):
-    pass
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()  
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+    print(add(3, 4))
+
 
 def halve(number):
-    pass
+    return number/2
+
+    print(halve(100))
